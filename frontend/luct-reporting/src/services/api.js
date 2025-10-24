3eb534faf5b41eed76d866420b35981ea0d61cb2
Jelani Lefatle <135925255+Smokey053@users.noreply.github.com>
2025-10-25 00:13:57 +0200

    Update API base URL to production endpoint

--- frontend/luct-reporting/src/services/api.js
+++ frontend/luct-reporting/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://luct-reporting-backend-vq3w.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
@@ -19,3 +19,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
