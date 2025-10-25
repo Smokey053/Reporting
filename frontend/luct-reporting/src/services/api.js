02ac1f5b65b89329b79ba6796ef3b67198031cc2
Jelani Lefatle <135925255+Smokey053@users.noreply.github.com>
2025-10-25 19:14:16 +0200

    Remove duplicate code in api.js

--- frontend/luct-reporting/src/services/api.js
+++ frontend/luct-reporting/src/services/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  import axios from "axios";
-
-const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://reporting-backend-1yy6.onrender.com/api",
 });
 
@@ -21,25 +18,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;,
-});
-
-api.interceptors.request.use((config) => {
-  if (typeof window === "undefined") {
-    return config;
-  }
-  const stored = window.localStorage.getItem("luct-reporting-session");
-  if (stored) {
-    const { token } = JSON.parse(stored);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  return config;
-});
-
 export default api;
-
-
-
-
